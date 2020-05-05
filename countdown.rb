ff6907074620e5e54291bbe7c71c2d9197812278
Uriah Harston <uharston@gmail.com>
2020-05-05 14:44:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,6 @@
 #write your code here
 
-def countdown
-  x = 10
+def countdown(x)
   while x > 0
     puts "#{x} SECOND(S)!"
     x -= 1
@@ -9,12 +8,12 @@ def countdown
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  x = 10
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
-    sleep(1)
-  end
-  puts "HAPPY NEW YEAR!"
-end
+# def countdown_with_sleep
+#   x = 10
+#   while x > 0
+#     puts "#{x} SECOND(S)!"
+#     x -= 1
+#     sleep(1)
+#   end
+#   puts "HAPPY NEW YEAR!"
+# end
