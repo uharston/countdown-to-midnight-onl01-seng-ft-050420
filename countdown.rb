f36d1e021ce1d2e72c7a7eee5dd57a90cda9895c
Uriah Harston <uharston@gmail.com>
2020-05-05 14:39:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,15 +6,15 @@ def countdown
     puts "#{x} SECOND(S)!"
     x -= 1
   end
-  return "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
 
-def countdown
+def countdown_with_sleep
   x = 10
   while x > 0
     puts "#{x} SECOND(S)!"
     x -= 1
     sleep(1)
   end
-  return "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
